fcea89c1ec2ec5c6594d6022b844b08fa884415f
luoyancn <luoyan_cn@outlook.com>
2019-11-21 21:52:53 +0800

    merge the projects

---
+++ opencvs/big_binary_image.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# 大尺寸图片的二值化
+
+import cv2 as cv
+import numpy as np
+
+
+def big_image_binary(img):
+    pass
+
+
+if __name__ == '__main__':
+    src = cv.imread('data/lena.jpg')
+
+    cv.waitKey(0)
+    cv.destroyAllWindows()
--- opencvs/rgb_video.py
+++ opencvs/rgb_video.py
@@ -12,7 +12,17 @@ while 1:
     if not ret:
         print('Cannot receive fram from camera, exiting...')
         break
-    cv.imshow('frame', frame)
+    frame = cv.flip(frame, 1)
+    gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    _, binary_frame = cv.threshold(gray_frame, 0, 255,
+                                     cv.THRESH_BINARY | cv.THRESH_OTSU)
+    _, trunc_binary = cv.threshold(gray_frame, 128, 255, cv.THRESH_TRUNC)
+    mean_binary = cv.adaptiveThreshold(gray_frame, 255,
+        cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 25, 10)
+    cv.imshow('rgb_frame', frame)
+    cv.imshow('binary_frame', binary_frame)
+    cv.imshow('trunc_frame', trunc_binary)
+    cv.imshow('mean_frame', mean_binary)
     if cv.waitKey(1) == ord('q'):
         break
 
